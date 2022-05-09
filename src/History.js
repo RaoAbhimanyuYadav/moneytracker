e510cbaff3801703a241fa68200a87fe60445032
RaoAbhimanyuYadav <raoyadavabhi@gmail.com>
2022-05-10 00:21:27 +0530

    Added delete trans

--- src/History.js
+++ src/History.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
 import "./history.css";
 import ClearAllIcon from "@mui/icons-material/ClearAll";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+
 const History = (props) => {
   const handleClear = () => {
-    localStorage.removeItem("moneytracker");
     props.clearStore();
   };
-
+  const [editHistory, setEditHistory] = useState(false);
+  const [idCheck, setIdCheck] = useState(null);
+  const handleEdit = (id) => {
+    console.log(id);
+  };
+  const handleDelete = (id) => {
+    props.deleteStore(id);
+  };
   return (
     <div className="history">
       <div className="icon-title">
@@ -20,8 +29,31 @@ const History = (props) => {
           props.state.map((detail) => {
             return (
               <li key={detail.key}>
-                <div className="transition">
+                <div
+                  className="transition"
+                  onMouseEnter={() => {
+                    setEditHistory(true);
+                    setIdCheck(detail.key);
+                  }}
+                  onMouseLeave={() => {
+                    setEditHistory(false);
+                  }}
+                >
                   <div className="trans-type">{detail.type}</div>
+                  {detail.key === idCheck && editHistory && (
+                    <div>
+                      <EditIcon
+                        onClick={() => {
+                          handleEdit(detail.key);
+                        }}
+                      />
+                      <DeleteIcon
+                        onClick={() => {
+                          handleDelete(detail.key);
+                        }}
+                      />
+                    </div>
+                  )}
                   <div className="trans-amount">
                     {detail.amount > 0 && "+"}
                     {detail.amount.toFixed(2)}
@@ -42,6 +74,9 @@ const mapReducerToProps = (dispatch) => {
   return {
     clearStore: () => {
       dispatch({ type: "CLEAR" });
+    },
+    deleteStore: (id) => {
+      dispatch({ type: "DELETE", key: id });
     },
   };
 };
--- src/store/store.js
+++ src/store/store.js
@@ -38,12 +38,38 @@ const reducer = (state = initailValue, action) => {
     return updatedState;
   }
   if (action.type === "CLEAR") {
+    localStorage.removeItem("moneytracker");
     return {
       transactions: [],
       amount: 0,
       income: 0,
       expense: 0,
     };
+  }
+  if (action.type === "DELETE") {
+    let newAmount = state.amount;
+    let newIncome = state.income;
+    let newExpense = state.expense;
+    const newTrans = state.transactions.filter((e) => {
+      if (e.key === action.key) {
+        newAmount -= e.amount;
+        if (e.color === "debit-color") {
+          newExpense += e.amount;
+        } else {
+          newIncome -= e.amount;
+        }
+      }
+      return e.key !== action.key;
+    });
+    const updatedState = {
+      ...state,
+      amount: newAmount,
+      income: newIncome,
+      expense: newExpense,
+      transactions: newTrans,
+    };
+    localStorage.setItem("moneytracker", JSON.stringify(updatedState));
+    return updatedState;
   }
 
   return state;
